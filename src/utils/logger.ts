67ab31436806d4beb937d038a4b204d7f1a0d10d
Ly Nguyen <lyntc.elec@gmail.com>
2021-01-05 21:26:51 +0700

    - Change logger, remove is-my-json-valid

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,24 +1,11 @@
 import { createLogger, format, transports } from "winston";
-import validator from "is-my-json-valid";
 
 const { splat, combine, timestamp, printf } = format;
 
-const validate = validator({
-  required: true,
-  type: "object",
-  properties: {
-    hello: {
-      required: true,
-      type: "string",
-    },
-  },
-});
-
 const myFormat = printf(({ timestamp, level, message, meta }: any) => {
-  if (validate(message)) {
-    message = JSON.stringify(message, null, 4);
-  }
-  return `${timestamp} ${level.toUpperCase()}  ${message} ${meta ? JSON.stringify(meta) : ""}`;
+  return `${timestamp} ${level.toUpperCase()}  ${JSON.stringify(message, null, 4)} ${
+    meta ? JSON.stringify(meta) : ""
+  }`;
 });
 
 const logger = createLogger({
