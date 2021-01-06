3e421860012b5d2da70640dd193d46d6f792c668
Ly Nguyen <lyntc.elec@gmail.com>
2021-01-06 08:33:45 +0700

    - Change passport flow, support GG Oauth2

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,81 +1,15 @@
 import express from "express";
 import passport from "passport";
-import { User } from "../lib/db";
-import status from "../lib/status";
-import * as encryption from "../lib/encryption";
-import * as mail from "../lib/mail";
-import * as jwttoken from "../lib/token";
+import { User } from "../models/db";
+import status from "../constants/status";
+import { genid } from "../utils/helper";
+import * as mail from "../utils/mail";
+import * as jwt from "../lib/jwt";
+import logger from "../utils/logger";
 
 const router = express.Router();
 
-router.post("/signup", async function (req, res) {
-  const checkuser: any = await User.findOne({ where: { email: req.body.email } });
-
-  if (checkuser !== null) {
-    if (checkuser.emailVerified) {
-      return res.json({
-        status: status.UNKNOWN,
-        msg: "Email already exists and register successfully",
-      });
-    } else {
-      await checkuser.destroy();
-    }
-  }
-
-  const emailVerifiedId = encryption.genid();
-  const user: any = new User();
-  user.username = req.body.username;
-  user.email = req.body.email;
-  user.password = req.body.password;
-  user.emailVerifiedId = emailVerifiedId;
-
-  await user.save();
-  const content = `
-  <h2 style="color: #2e6c80;">Click on this link to verify your email:</h2>
-  <p><strong>&nbsp;<a href="https://${process.env.HOST}/verifyemail?id=${emailVerifiedId}">https://${process.env.HOST}/verifyemail?id=${emailVerifiedId}</a></strong></p>
-    `;
-  mail.send(req.body.email, "iLogger Email Verification Code", content);
-
-  if (checkuser !== null && checkuser.emailVerified === false) {
-    return res.json({
-      status: status.UNKNOWN,
-      msg: "Email already exists but has not been verified, please check your mailbox",
-    });
-  } else {
-    return res.json({
-      status: status.SUCCESS,
-      msg: "Signup successfully, please check your mailbox to verify your email",
-    });
-  }
-});
-
-router.post("/login", function (req, res, next) {
-  passport.authenticate("local", { session: false }, function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (user.emailVerified === false) {
-      return res.json({
-        status: status.UNKNOWN,
-        msg: "Email already exists but has not been verified, please check your mailbox",
-      });
-    } else if (user) {
-      user.token = jwttoken.generateJWT(user);
-      return res.json({
-        status: status.SUCCESS,
-        msg: "Login successfully",
-        user: jwttoken.toAuthJSON(user),
-      });
-    } else {
-      return res.json({
-        status: status.UNKNOWN,
-        msg: info.message,
-      });
-    }
-  })(req, res, next);
-});
-
-router.post("/verifyemail", async function (req, res) {
+router.post("/verifyemail", async function (req: any, res: any) {
   const user: any = await User.findOne({
     where: { emailVerifiedId: req.body.VerifyEmailId },
   });
@@ -100,6 +34,125 @@ router.post("/verifyemail", async function (req, res) {
   return res.json({
     status: status.SUCCESS,
     msg: "Email verification done",
+  });
+});
+
+router.post("/signup", async function (req: any, res: any) {
+  if (req.body.password !== req.body.passwordConfirm) {
+    return res.json({
+      status: status.PASS_NOT_MATCHED,
+      msg: "Password confirm do not match",
+    });
+  }
+
+  const checkuser: any = await User.findOne({ where: { email: req.body.email } });
+  if (checkuser !== null) {
+    if (checkuser.emailVerified) {
+      return res.json({
+        status: status.EMAIL_ALREADY_EXISTS,
+        msg: "Email already exists and register successfully",
+      });
+    } else {
+      await checkuser.destroy();
+    }
+  }
+
+  const emailVerifiedId = genid().replace(/-/g, "");
+
+  const user: any = new User();
+  user.displayName = req.body.displayName;
+  user.givenName = "";
+  user.familyName = "";
+  user.email = req.body.email;
+  user.password = req.body.password;
+  user.emailVerifiedId = emailVerifiedId;
+  await user.save();
+
+  // const content = `
+  // <h2 style="color: #2e6c80;">Click on this link to verify your email:</h2>
+  // <p><strong>&nbsp;<a href="${process.env.BASE_URL}/verifyemail?id=${emailVerifiedId}">${process.env.BASE_URL}/verifyemail?id=${emailVerifiedId}</a></strong></p>
+  // `;
+  // mail.send(req.body.email, "iLogger Email Verification", content);
+
+  if (checkuser !== null && checkuser.emailVerified === false) {
+    return res.json({
+      status: status.UNKNOWN,
+      msg: "Email already exists but has not been verified, please check your mailbox",
+    });
+  }
+
+  return res.json({
+    status: status.SUCCESS,
+    msg: "Signup successfully, please check your mailbox to verify your email",
+  });
+});
+
+router.post("/login", function (req, res, next) {
+  passport.authenticate("local", { session: false }, function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (user.emailVerified === false) {
+      return res.json({
+        status: status.ERROR,
+        code: status.EMAIL_NOT_VERIFIED,
+        msg: "Email already exists but has not been verified, please check your mailbox",
+      });
+    } else if (user) {
+      let access_token = jwt.generateJWT(user);
+      return res.json({
+        status: status.SUCCESS,
+        msg: "Login successfully",
+        payload: {
+          access_token: access_token,
+        },
+      });
+    } else if (user === null) {
+      return res.json({
+        status: status.ERROR,
+        code: status.LOGIN_FAILED,
+        msg: info.message,
+      });
+    }
+  })(req, res, next);
+});
+
+router.get("/user", async function (req: any, res: any) {
+  return res.json({
+    status: status.SUCCESS,
+    msg: "user",
+    user: req.user,
+  });
+});
+
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: `${process.env.API_PATH}/auth/google/success`,
+    failureRedirect: `${process.env.API_PATH}/auth/google/failure`,
+  }),
+);
+
+router.get("/google/success", async function (req: any, res: any) {
+  req.user.bio = null;
+  req.user.image = null;
+  req.user.jwt = jwt.toAuthJSON(req.user);
+  return res.json({
+    status: 0,
+    msg: "Google login successfully",
+    data: {
+      user: req.user.jwt,
+    },
+  });
+});
+
+router.get("/google/failure", async function (req: any, res: any) {
+  return res.json({
+    status: status.ERROR,
+    code: status.GG_OAUTH_FAILED,
+    msg: "Google login error",
   });
 });
 
--- src/passport/setup.ts
+++ src/passport/setup.ts
@@ -1,31 +1,69 @@
-import { User } from "../lib/db";
 import passport from "passport";
 import passportLocal from "passport-local";
+import passportGoogle from "passport-google-oauth2";
+import { User } from "../models/db";
+import logger from "../utils/logger";
+import * as jwt from "../lib/jwt";
+import status from "../constants/status";
 
 const LocalStrategy = passportLocal.Strategy;
+const GoogleStrategy = passportGoogle.Strategy;
 
-passport.serializeUser((user: any, done: any) => {
-  done(null, user.id);
+passport.serializeUser(async function (user: any, done: any) {
+  logger.info(`serializeUser ${user.email}`);
+  const CurrentUser: any = await User.findOne({ where: { email: user.email } });
+  console.log("id", CurrentUser.id);
+  done(null, CurrentUser.id);
 });
 
-passport.deserializeUser((id: any, done: any) => {
-  // User.findById(id, (err: any, user: any) => {
-  //   done(err, user);
-  // });
+passport.deserializeUser(async function (id: any, done: any) {
+  console.log(`deserializeUser ${id}`);
+  const CurrentUser: any = await User.findOne({ where: { id: id } });
+  done(null, CurrentUser.dataValues);
 });
 
 passport.use(
-  new LocalStrategy({ usernameField: "email" }, async (email: any, password: any, done: any) => {
-    // const user = await User.findOne({ where: { email: email } });
-    // // if (err) { return done(err) }
-    // if (user === null) {
-    //   return done(null, false, { message: "Incorrect email." });
-    // }
-    // if (!jwttoken.validPassword(user, password)) {
-    //   return done(null, false, { message: "Incorrect password." });
-    // }
-    // return done(null, user);
-  }),
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email: string, password: string, done: any) => {
+      const user: any = await User.findOne({ where: { email: email } });
+      if (user === null) {
+        return done(null, false, { code: status.EMAIL_INVALID, msg: "Incorrect email." });
+      } else if (user.emailVerified === false) {
+        return done(null, false, {
+          message: "Email already exists but has not been verified, please check your mailbox.",
+        });
+      } else if (!jwt.validPassword(user.salt, password, user.password)) {
+        return done(null, false, { code: status.PASS_NOT_MATCHED, msg: "Incorrect password." });
+      }
+      return done(null, user);
+    },
+  ),
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL: `${process.env.API_PATH}/auth/google/callback`,
+      passReqToCallback: true,
+    },
+    async function (request: any, accessToken: any, refreshToken: any, profile: any, done: any) {
+      const user = await User.findOne({ where: { email: profile.email } });
+      if (user === null) {
+        const newuser: any = new User();
+        newuser.displayName = profile.displayName;
+        newuser.givenName = profile.given_name;
+        newuser.familyName = profile.family_name;
+        newuser.email = profile.email;
+        newuser.emailVerified = profile.email_verified;
+        await newuser.save();
+        return done(null, newuser);
+      }
+      return done(null, user);
+    },
+  ),
 );
 
 export = passport;
